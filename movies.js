35b0e3cef667ace65c6e42df4dcbed6ea5dacbab
KelsieHargreaves <kelsie.hargreaves@yahoo.com>
2025-08-28 13:11:55 -0500

    fixed changes

--- movies.js
+++ movies.js
@@ -1,7 +1,17 @@
 const moviesWrapper = document.querySelector('.movies')
 const searchName = document.querySelector('.search__name')
+const loadingBar = document.querySelector('.loading__bar')
 
 function searchChange(event) {
+    const searchTerm = event.target.value
+    
+    if (searchTerm) {
+        loadingBar.classList.add('searching')
+    } 
+    else {
+        loadingBar.classList.remove('searching')
+    }
+    
     renderMovies(event.target.value)
     searchName.innerHTML = event.target.value
 
@@ -11,15 +21,21 @@ let currentMovies = []
 
 async function renderMovies(searchTerm) {
     const response = await fetch(
-        `https://www.omdbapi.com/?s=${searchTerm}&apikey=5be1f0a6`
-    )
-    const data = await response.json()
-    currentMovies = data.Search
-    displayMovies(currentMovies)
-}
+            `https://www.omdbapi.com/?s=${searchTerm}&apikey=5be1f0a6`
+        )
+        const data = await response.json()
+        currentMovies = data.Search
+
+        
+        setTimeout (() => {
+            displayMovies(currentMovies)
+            loadingBar.classList.remove('searching')
+        }, 3000)
+}    
 
 function displayMovies(movieList) {
     moviesWrapper.innerHTML = movieList
+    .slice(0, 6)
     .map((movie) => {
         return `
         <div class="movie">
