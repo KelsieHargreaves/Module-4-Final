3a92a2afc8fbd82d941f7c368c2e42065a7ccb80
KelsieHargreaves <kelsie.hargreaves@yahoo.com>
2025-08-28 22:10:06 -0500

    added home screen

---
+++ script.js
@@ -0,0 +1,10 @@
+function openSpinner(button) {
+    button.classList.add("loading");
+    button.disabled = true;
+
+    setTimeout(() => {
+        window.location.href = `./movies.html`;
+    }, 300)
+
+}
+
